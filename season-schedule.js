ff47caab33f63b6755560033374003af65338e6b
Kyle Cooke <kylebcooke@outlook.com>
2026-04-22 00:06:58 -0400

    ...

--- season-schedule.js
+++ season-schedule.js
@@ -30,6 +30,180 @@ window.HFF_SCHEDULE = {
             homeScore: "1",
             datetime: "9:30 AM",
             field: "Field 1"
+          },
+          {
+            away: "Browns (U12)",
+            awayLogo: "White Logo.jpg",
+            awayRecord: "",
+            awayScore: "0",
+            home: "Jets (U12)",
+            homeLogo: "White Logo.jpg",
+            homeRecord: "",
+            homeScore: "2",
+            datetime: "10:00 AM",
+            field: "Field 1"
+          },
+          {
+            away: "Bears (U9)",
+            awayLogo: "White Logo.jpg",
+            awayRecord: "",
+            awayScore: "4",
+            home: "Saints (U9)",
+            homeLogo: "White Logo.jpg",
+            homeRecord: "",
+            homeScore: "3",
+            datetime: "10:30 AM",
+            field: "Field 1"
+          },
+          {
+            away: "Bills (U12)",
+            awayLogo: "White Logo.jpg",
+            awayRecord: "",
+            awayScore: "6",
+            home: "Ravens (U12)",
+            homeLogo: "White Logo.jpg",
+            homeRecord: "",
+            homeScore: "5",
+            datetime: "11:00 AM",
+            field: "Field 1"
+          },
+          {
+            away: "Chiefs (U16)",
+            awayLogo: "White Logo.jpg",
+            awayRecord: "",
+            awayScore: "4",
+            home: "Cowboys (U16)",
+            homeLogo: "White Logo.jpg",
+            homeRecord: "",
+            homeScore: "6",
+            datetime: "11:30 AM",
+            field: "Field 1"
+          },
+          {
+            away: "Panthers (U16)",
+            awayLogo: "White Logo.jpg",
+            awayRecord: "",
+            awayScore: "2",
+            home: "Patriots (U16)",
+            homeLogo: "White Logo.jpg",
+            homeRecord: "",
+            homeScore: "23",
+            datetime: "12:00 PM",
+            field: "Field 1"
+          },
+          {
+            away: "Cowboys (U16)",
+            awayLogo: "White Logo.jpg",
+            awayRecord: "",
+            awayScore: "65",
+            home: "Commanders (U16)",
+            homeLogo: "White Logo.jpg",
+            homeRecord: "",
+            homeScore: "25",
+            datetime: "12:30 PM",
+            field: "Field 1"
+          }
+        ]
+      },
+      {
+        weekTitle: "Week 2",
+        weekDate: "Saturday, May 23, 2026",
+        games: [
+          {
+            away: "Giants (U6)",
+            awayLogo: "White Logo.jpg",
+            awayRecord: "",
+            awayScore: "",
+            home: "Giants (U6)",
+            homeLogo: "White Logo.jpg",
+            homeRecord: "",
+            homeScore: "",
+            datetime: "9:15 AM",
+            field: "Field 1"
+          },
+          {
+            away: "Bears (U9)",
+            awayLogo: "White Logo.jpg",
+            awayRecord: "",
+            awayScore: "",
+            home: "Packers (U9)",
+            homeLogo: "White Logo.jpg",
+            homeRecord: "",
+            homeScore: "",
+            datetime: "9:30 AM",
+            field: "Field 1"
+          },
+          {
+            away: "Jets (U12)",
+            awayLogo: "White Logo.jpg",
+            awayRecord: "",
+            awayScore: "",
+            home: "Bills (U12)",
+            homeLogo: "White Logo.jpg",
+            homeRecord: "",
+            homeScore: "",
+            datetime: "10:00 AM",
+            field: "Field 1"
+          },
+          {
+            away: "Saints (U9)",
+            awayLogo: "White Logo.jpg",
+            awayRecord: "",
+            awayScore: "",
+            home: "Packers (U9)",
+            homeLogo: "White Logo.jpg",
+            homeRecord: "",
+            homeScore: "",
+            datetime: "10:30 AM",
+            field: "Field 1"
+          },
+          {
+            away: "Ravens (U12)",
+            awayLogo: "White Logo.jpg",
+            awayRecord: "",
+            awayScore: "",
+            home: "Browns (U12)",
+            homeLogo: "White Logo.jpg",
+            homeRecord: "",
+            homeScore: "",
+            datetime: "11:00 AM",
+            field: "Field 1"
+          },
+          {
+            away: "Panthers (U16)",
+            awayLogo: "White Logo.jpg",
+            awayRecord: "",
+            awayScore: "",
+            home: "Chiefs (U16)",
+            homeLogo: "White Logo.jpg",
+            homeRecord: "",
+            homeScore: "",
+            datetime: "11:30 AM",
+            field: "Field 1"
+          },
+          {
+            away: "Patriots (U16)",
+            awayLogo: "White Logo.jpg",
+            awayRecord: "",
+            awayScore: "",
+            home: "Cowboys (U16)",
+            homeLogo: "White Logo.jpg",
+            homeRecord: "",
+            homeScore: "",
+            datetime: "12:00 PM",
+            field: "Field 1"
+          },
+          {
+            away: "Chiefs (U16)",
+            awayLogo: "White Logo.jpg",
+            awayRecord: "",
+            awayScore: "",
+            home: "Commanders (U16)",
+            homeLogo: "White Logo.jpg",
+            homeRecord: "",
+            homeScore: "",
+            datetime: "12:30 PM",
+            field: "Field 1"
           }
         ]
       }
